90fbd078f2e74fa4dfe684bb4e870895b0fc7665
maicol.nicolini <maicol.nicolini@accenture.com>
2024-01-23 10:54:41 +0100

    resolved initialization bug

--- utils/costFunctions.py
+++ utils/costFunctions.py
@@ -17,6 +17,7 @@ def cross_entropy_cost(AL, Y):
 
     # Compute loss from aL and y.
     cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T))
+    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T))
     cost = np.squeeze(cost)  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
 
 
