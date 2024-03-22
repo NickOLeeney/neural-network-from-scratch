fixed some softmax minor bugs, still not working

utils/costFunctions.py
@@ -62,7 +62,8 @@ def cross_entropy_cost_softmax(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    cost = (1. / m) * ((-Y * np.log(AL)) - (1 - Y) * np.log(1 - AL)).sum(axis=1).sum()
+    # cost = (1. / m) * ((-Y * np.log(AL)) - (1 - Y) * np.log(1 - AL)).sum(axis=1).sum()
+    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T)).sum(axis=0).sum()
 
     cost = np.squeeze(cost)  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
 