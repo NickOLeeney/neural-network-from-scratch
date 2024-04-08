a7fa358aa57a9537d001eccdf949c3af5e6364ef
maicol.nicolini <maicol.nicolini@accenture.com>
2024-04-08 13:02:02 +0200

    checkpoint

--- utils/costFunctions.py
+++ utils/costFunctions.py
@@ -17,7 +17,7 @@ def cross_entropy_cost(AL, Y):
     m = Y.shape[1]
 
     # Compute loss from aL and y.
-    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T))
+    cost = (1. / m) * (-np.dot(Y, np.log(AL+1e-9).T) - np.dot(1 - Y, np.log(1 - AL+1e-9).T))
     cost = np.squeeze(cost)  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
 
     return cost
